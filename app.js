69bc9d931ba3e482a84bf6761102326e0653c680
Tahir Ovayurt <iovayurt@gmail.com>
2022-01-21 18:14:39 +0300

    controls

--- app.js
+++ app.js
@@ -2,12 +2,14 @@ document.addEventListener('DOMContentLoaded', () => {
   const grid = document.querySelector('.grid')
   const doodler = document.createElement('div')
   let doodlerLeftSpace = 50
-  let doodlerBottomSpace = 50
+  let startPoint = 150
+  let doodlerBottomSpace = startPoint
   let isGameOver = false
   let platformCount = 5
   let platforms = []
   let upTimerId
   let downTimerId
+  let isJumping = true
 
   function createDoodler() {
     grid.appendChild(doodler)
@@ -54,10 +56,11 @@ document.addEventListener('DOMContentLoaded', () => {
 
   function jump() {
     clearInterval(downTimerId)
+    isJumping = true
     upTimerId = setInterval(function () {
       doodlerBottomSpace += 20
       doodler.style.bottom = doodlerBottomSpace + 'px'
-      if (doodlerBottomSpace > 350) {
+      if (doodlerBottomSpace > startPoint + 200) {
         fall()
       }
     },30)
@@ -65,12 +68,28 @@ document.addEventListener('DOMContentLoaded', () => {
 
   function fall() {
     clearInterval(upTimerId)
+    isJumping = false
     downTimerId = setInterval(function () {
       doodlerBottomSpace -= 5
       doodler.style.bottom = doodlerBottomSpace + 'px'
       if (doodlerBottomSpace <= 0) {
         gameOver()
       }
+
+      platforms.forEach(platform => {
+        if (
+          (doodlerBottomSpace >= platform.bottom) &&
+          (doodlerBottomSpace <= platform.bottom + 15) &&
+          ((doodlerBottomSpace + 60) >= platform.left) &&
+          (doodlerBottomSpace <= (platform.left + 85)) &&
+          !isJumping
+        ){
+          console.log('landed')
+          startPoint = doodlerBottomSpace
+          jump()
+        }
+      })
+
     },30)
   }
 
@@ -79,6 +98,16 @@ document.addEventListener('DOMContentLoaded', () => {
     isGameOver = true
     clearInterval(upTimerId)
     clearInterval(downTimerId)
+  }
+
+  function control(e) {
+    if (e.key === "ArrowLeft") {
+      //move left
+    } else if (e.key === "ArrowRight") {
+      // move right
+    } else if (e.key === "ArrowUp") {
+      //move straight
+    }
   }
 
   function start() {
